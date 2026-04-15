1905657919fbdefc3b6fc50d2de32577d70b481a
berosee <berosegoo@gmail.com>
2026-04-15 13:45:12 +0900

    202604157

---
+++ playlist_data.js
@@ -0,0 +1,124 @@
+const playlists = {
+    "enka": [
+        {
+            "title": "016 No. 3 in D minor - 2. Tempo di Menuetto. Sehr masig",
+            "url": "music/enka/016 No. 3 in D minor - 2. Tempo di Menuetto. Sehr masig.flac"
+        },
+        {
+            "title": "018 No. 3 in D minor - 4. Sehr langsam. Misterioso. Durchaus ppp O Mensch! Gib acht!",
+            "url": "music/enka/018 No. 3 in D minor - 4. Sehr langsam. Misterioso. Durchaus ppp O Mensch! Gib acht!.flac"
+        },
+        {
+            "title": "019 No. 3 in D minor - 5. Lustig im Tempo und keck im Ausdruck Bimm bammEs sungen drei Engel",
+            "url": "music/enka/019 No. 3 in D minor - 5. Lustig im Tempo und keck im Ausdruck Bimm bammEs sungen drei Engel.flac"
+        },
+        {
+            "title": "021 No. 4 in G major - 1. Bedachtig. Nicht eilen",
+            "url": "music/enka/021 No. 4 in G major - 1. Bedachtig. Nicht eilen.flac"
+        },
+        {
+            "title": "022 No. 4 in G major - 2. In gemachlicher Bewegung. Ohne Hast",
+            "url": "music/enka/022 No. 4 in G major - 2. In gemachlicher Bewegung. Ohne Hast.flac"
+        },
+        {
+            "title": "023 No. 4 in G major - 3. Ruhevoll. Poco Adagio",
+            "url": "music/enka/023 No. 4 in G major - 3. Ruhevoll. Poco Adagio.flac"
+        },
+        {
+            "title": "024 No. 4 in G major - 4. Sehr behaglich Wir geniesen die himmlischen Freuden",
+            "url": "music/enka/024 No. 4 in G major - 4. Sehr behaglich Wir geniesen die himmlischen Freuden.flac"
+        },
+        {
+            "title": "Four Seasons 'Autumn'-  Allegro (La Caccia)",
+            "url": "music/enka/Four Seasons 'Autumn'-  Allegro (La Caccia).mp3"
+        },
+        {
+            "title": "Four Seasons 'Autumn'- Adagio",
+            "url": "music/enka/Four Seasons 'Autumn'- Adagio.mp3"
+        },
+        {
+            "title": "Four Seasons 'Autumn'- Allegro",
+            "url": "music/enka/Four Seasons 'Autumn'- Allegro.mp3"
+        },
+        {
+            "title": "Four Seasons 'Spring'- Allegro (danza)",
+            "url": "music/enka/Four Seasons 'Spring'- Allegro (danza).mp3"
+        },
+        {
+            "title": "Four Seasons 'Spring'- Allegro",
+            "url": "music/enka/Four Seasons 'Spring'- Allegro.mp3"
+        },
+        {
+            "title": "Four Seasons 'Spring'- Largo e pianissimo",
+            "url": "music/enka/Four Seasons 'Spring'- Largo e pianissimo.mp3"
+        }
+    ],
+    "jazz": [
+        {
+            "title": "Andy Williams - Love Is Blue",
+            "url": "music/jazz/Andy Williams - Love Is Blue.flac"
+        },
+        {
+            "title": "Barbra Streisand - Woman In Love",
+            "url": "music/jazz/Barbra Streisand - Woman In Love.flac"
+        },
+        {
+            "title": "신사동 그사람",
+            "url": "music/jazz/신사동 그사람.mp3"
+        }
+    ],
+    "old": [
+        {
+            "title": "Beyonce - TEXAS HOLD EM",
+            "url": "music/old/Beyonce - TEXAS HOLD EM.mp3"
+        },
+        {
+            "title": "Daniel - Blackboard of My Heart",
+            "url": "music/old/Daniel - Blackboard of My Heart.mp3"
+        },
+        {
+            "title": "Daniel - Pretty Little Girl From Omagh",
+            "url": "music/old/Daniel - Pretty Little Girl From Omagh.mp3"
+        },
+        {
+            "title": "Green Green Grass Of Home",
+            "url": "music/old/Green Green Grass Of Home.mp3"
+        },
+        {
+            "title": "Isla Grant - Every Moment Of Every Hour",
+            "url": "music/old/Isla Grant - Every Moment Of Every Hour.mp3"
+        },
+        {
+            "title": "Isla Grant - Only Yesterday",
+            "url": "music/old/Isla Grant - Only Yesterday.mp3"
+        },
+        {
+            "title": "Jonny Cash - Cotton Fields",
+            "url": "music/old/Jonny Cash - Cotton Fields.mp3"
+        },
+        {
+            "title": "Mary Duff - Just Lovin' You",
+            "url": "music/old/Mary Duff - Just Lovin' You.mp3"
+        },
+        {
+            "title": "Mary Duff - Power Of Love",
+            "url": "music/old/Mary Duff - Power Of Love.mp3"
+        },
+        {
+            "title": "Mary Duff - Will You Walk With Me",
+            "url": "music/old/Mary Duff - Will You Walk With Me.mp3"
+        },
+        {
+            "title": "The Carpenters - Yesterday Once More",
+            "url": "music/old/The Carpenters - Yesterday Once More.mp3"
+        },
+        {
+            "title": "Tom Jones - Delilah",
+            "url": "music/old/Tom Jones - Delilah.mp3"
+        },
+        {
+            "title": "dave - The Happy Organ",
+            "url": "music/old/dave - The Happy Organ.flac"
+        }
+    ]
+};
